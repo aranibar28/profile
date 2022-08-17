0e0c99ab17f44ab53d1f0125793b72a4194f775f
Gerson Aranibar <aranibargerson28@gmail.com>
2022-08-17 00:02:25 -0500

    add pipe

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { ImagePipe } from '../pipes/image.pipe';
 import { SharedModule } from '../shared/shared.module';
 import { PagesComponent } from './pages.component';
 
@@ -12,6 +13,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProcessComponent } from './process/process.component';
 
+
 @NgModule({
   declarations: [
     PagesComponent,
@@ -20,12 +22,14 @@ import { ProcessComponent } from './process/process.component';
     AboutComponent,
     ContactComponent,
     ProcessComponent,
+    ImagePipe,
   ],
   imports: [
     CommonModule,
     SharedModule,
     RouterModule,
     ReactiveFormsModule,
+    
   ],
 })
 export class PagesModule {}
---
+++ src/app/pipes/image.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'image',
+})
+export class ImagePipe implements PipeTransform {
+  transform(value: string): any {
+    return 'assets/img/icons/' + value + '.svg';
+  }
+}
